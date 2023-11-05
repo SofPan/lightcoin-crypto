d1179b2dc20fe4499e57a0c257eca9cf399ae27d
Sofia Panchaud <s.panchaud.89@gmail.com>
2023-11-05 16:24:53 -0500

    added transaction history and balance check to Account class

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
-let balance = 500.00;
+// let balance = 500.00;
 
 class Account {
 
   constructor(username) {
     this.username = username;
     this.balance = 0;
+    this.transactions = [];
+  }
+
+  addTransaction(transaction) {
+    this.transactions.push(transaction);
+    this.transactions;
+  }
+
+  getBalance() {
+    for (const transaction of this.transactions) {
+      this.balance += transaction.amount;
+    }
+    return this.balance;
   }
 }
 
@@ -15,7 +28,9 @@ class Transaction {
   }
 
   commit() {
-    this.account.balance += this.value;
+    this.amount = this.value;
+    this.time = new Date();
+    this.account.addTransaction(this);
   }
 }
 class Withdrawal extends Transaction {
@@ -41,14 +56,16 @@ const myAccount = new Account("snow-patrol");
 
 const t1 = new Withdrawal(50.25, myAccount);
 t1.commit();
-console.log('Transaction 1:', t1);
+// console.log('Transaction 1:', t1);
 
 const t2 = new Withdrawal(9.99, myAccount);
 t2.commit();
-console.log('Transaction 2:', t2);
+// console.log('Transaction 2:', t2);
 
-console.log('Balance:', balance);
+// console.log('Balance:', balance);
 
 const t3 = new Deposit(120.00, myAccount);
 t3.commit();
-console.log('Transaction 3:', t3);
+// console.log('Transaction 3:', t3);
+
+console.log("getting my balance", myAccount.getBalance());
